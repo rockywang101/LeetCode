685d295690b9596488a3066ffeeca78a10450252
rocky.wang <rockywang101@gmail.com>
2019-11-04 02:02:36 +0800

    重新寫 A0283

--- src/main/java/LeetCode/A0283_MoveZeroes.java
+++ src/main/java/LeetCode/A0283_MoveZeroes.java
@@ -6,13 +6,57 @@ import java.util.List;
 public class A0283_MoveZeroes {
 
 	/**
+	 * @param nums
+	 */
+	// Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+	// Memory Usage: 37.4 MB, less than 100.00% of Java online submissions for Move Zeroes.
+	// 其實記憶體沒有用的比較少，這版第一次用了 38.4 MB，再 submit 一次就變 37.4 MB
+    public void moveZeroes(int[] nums) {
+    	
+    	int pointer = 0;
+    	for (int i=0; i<nums.length; i++) {
+    		
+    		if (nums[i] != 0) {
+    			
+    			if (pointer != i) {
+    				nums[pointer] = nums[i];
+    				nums[i] = 0;
+    			}
+    			pointer++;
+    		}
+    	}
+    }
+	
+	/**
+	 * 這版有點繞圈了
+	 * @param nums
+	 */
+	// Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+	// Memory Usage: 38.1 MB, less than 81.82% of Java online submissions for Move Zeroes.
+    public void moveZeroesV2(int[] nums) {
+    	
+    	int pointer = 0;
+    	for (int i=0; i<nums.length; i++) {
+    		
+    		if (nums[i] != 0 && nums[pointer] == 0 && pointer != i) {
+    			nums[pointer] = nums[i]; // pointer 與 i 的值互換
+    			nums[i] = 0; // 要設成 nums[pointer] 的值，其實就是 0 
+    			pointer++;
+    		}
+    		else if (nums[i]== 0 && nums[pointer] != 0) {
+    			pointer = i;
+    		}
+    	}
+    }
+	
+	/**
 	 * 有點取巧的意味，因為題目說不能有一個 copy 的 array
 	 * 但沒說到不能用另一個 list 來記錄
 	 * @param nums
 	 */
 	// Runtime: 1 ms, faster than 18.61% of Java online submissions for Move Zeroes.
 	// Memory Usage: 38.6 MB, less than 78.32% of Java online submissions for Move Zeroes.
-    public void moveZeroes(int[] nums) {
+    public void moveZeroesV1(int[] nums) {
         // 取出非 0 的數字
     	List<Integer> numList = new LinkedList<>();
     	for (int num : nums) {
--- src/test/java/LeetCode/A0283_MoveZeroesTest.java
+++ src/test/java/LeetCode/A0283_MoveZeroesTest.java
@@ -1,7 +1,6 @@
 package LeetCode;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,5 +15,20 @@ public class A0283_MoveZeroesTest {
 		obj.moveZeroes(nums);
 		assertArrayEquals(new int[] {1, 3, 12, 0, 0}, nums);
 		
+		nums = new int[] {0, 0, 0, 1};
+		obj.moveZeroes(nums);
+		assertArrayEquals(new int[] {1, 0, 0, 0}, nums);
+		
+		nums = new int[] {3, 2, 0, 0, 1};
+		obj.moveZeroes(nums);
+		assertArrayEquals(new int[] {3, 2, 1, 0, 0}, nums);
+		
+		nums = new int[] {3, 2, 0, 0, 1, 0};
+		obj.moveZeroes(nums);
+		assertArrayEquals(new int[] {3, 2, 1, 0, 0, 0}, nums);
+		
+		nums = new int[] {3, 0, 0, 1, 2};
+		obj.moveZeroes(nums);
+		assertArrayEquals(new int[] {3, 1, 2, 0, 0}, nums);
 	}
 }

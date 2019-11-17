7ea8d616ec68f900661ad4791b4ca6bdee509ff7
rocky.wang <rockywang101@gmail.com>
2019-11-17 23:54:46 +0800

    refine A0007

--- src/main/java/LeetCode/A0007_ReverseInteger.java
+++ src/main/java/LeetCode/A0007_ReverseInteger.java
@@ -1,32 +1,56 @@
 package LeetCode;
 
+
 public class A0007_ReverseInteger {
 
-	public int reverse(int x) {
-	    
-		boolean isNegative = x < 0;
-		if (isNegative)
-			x = -x;
-		
+	// Runtime: 1 ms, faster than 100.00% of Java online submissions for Reverse Integer.
+	// Memory Usage: 33.7 MB, less than 11.66% of Java online submissions for Reverse Integer.
+    public int reverse(int x) {
+        
 		int ret = 0;
-		while (x > 0) {
-		
-			System.out.print(x + "\t");
-			int pop = x % 10;
+		while (x != 0) {
+			int pop = x % 10; // 取尾數
 			x = x / 10;
 			
 			ret = ret * 10 + pop;
-			
-			System.out.print(pop + "\t" + x + "\t" + ret);
-			System.out.println();
+			if (x > 0 && ret > Integer.MAX_VALUE / 10) {
+				return 0;
+			}
+			else if (x < 0 && ret < Integer.MIN_VALUE / 10) {
+				return 0;
+			}
 		}
-		
-		
-		if (isNegative)
-			ret = -ret;
 		
 		return ret;
     }
+	
+	// println 沒拿掉，分數會很差
+	// Runtime: 22 ms, faster than 5.10% of Java online submissions for Reverse Integer.
+	// Memory Usage: 35.7 MB, less than 5.55% of Java online submissions for Reverse Integer.
+	public int reverseV2(int x) {
+		System.out.println("input x => " + x);
+		
+		int ret = 0;
+		while (x != 0) {
+			int pop = x % 10; // 取尾數
+			x = x / 10;
+			
+			ret = ret * 10 + pop;
+			System.out.println(x + "\tpop:\t" + pop + "\tret:\t" + ret);
+			
+			if (x > 0 && ret > Integer.MAX_VALUE / 10) {
+				System.out.println("ret " + ret + " is over, return 0");
+				return 0;
+			}
+			else if (x < 0 && ret < Integer.MIN_VALUE / 10) {
+				System.out.println("ret " + ret + " is over , return 0");
+				return 0;
+			}
+		}
+		
+		return ret;
+    }
+	
 	
 	// Runtime: 2 ms, faster than 24.19% of Java online submissions for Reverse Integer.
 	// Memory Usage: 33.9 MB, less than 9.44% of Java online submissions for Reverse Integer.
@@ -58,23 +82,17 @@ public class A0007_ReverseInteger {
 		}
     }
 	
+	
 	public static void main(String[] args) {
 		
-		short x = 357;
+		System.out.println(new A0007_ReverseInteger().reverse(0));
 		
-		int r = new A0007_ReverseInteger().reverse(x);
-		System.out.println(r);
+		System.out.println(new A0007_ReverseInteger().reverse(12345));
 		
-//		System.out.println(Short.MAX_VALUE); // 32767
-//		System.out.println(Short.MIN_VALUE); // -32768
-//
-//		System.out.println(Integer.MAX_VALUE); // 2147483647
-//		System.out.println(Integer.MIN_VALUE); // -2147483648
-//		long sum = 1;
-//		for (int i=1; i<=31; i++) {
-//		    sum = sum * 2;
-//			System.out.println(i + "\t" + sum);
-//		}
+		System.out.println(new A0007_ReverseInteger().reverse(-12345));
 		
+		System.out.println(new A0007_ReverseInteger().reverse(2147483647));
+		
+		System.out.println(new A0007_ReverseInteger().reverse(-2147483648));
 	}
 }

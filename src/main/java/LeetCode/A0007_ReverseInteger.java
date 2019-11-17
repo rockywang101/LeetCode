ba8e853c0807d023c2fc61f04add41ec3f60ed4a
rocky <rocky@LAPTOP-E0RDIJNS>
2019-11-17 22:42:42 +0800

    refactor A0007

--- src/main/java/LeetCode/A0007_ReverseInteger.java
+++ src/main/java/LeetCode/A0007_ReverseInteger.java
@@ -2,9 +2,35 @@ package LeetCode;
 
 public class A0007_ReverseInteger {
 
+	public int reverse(int x) {
+	    
+		boolean isNegative = x < 0;
+		if (isNegative)
+			x = -x;
+		
+		int ret = 0;
+		while (x > 0) {
+		
+			System.out.print(x + "\t");
+			int pop = x % 10;
+			x = x / 10;
+			
+			ret = ret * 10 + pop;
+			
+			System.out.print(pop + "\t" + x + "\t" + ret);
+			System.out.println();
+		}
+		
+		
+		if (isNegative)
+			ret = -ret;
+		
+		return ret;
+    }
+	
 	// Runtime: 2 ms, faster than 24.19% of Java online submissions for Reverse Integer.
 	// Memory Usage: 33.9 MB, less than 9.44% of Java online submissions for Reverse Integer.
-	public int reverse(int x) {
+	public int reverseV1(int x) {
     
 		boolean isNegative = x < 0;
 		if (isNegative)
@@ -34,9 +60,21 @@ public class A0007_ReverseInteger {
 	
 	public static void main(String[] args) {
 		
-		int x = 1534236469;
+		short x = 357;
 		
 		int r = new A0007_ReverseInteger().reverse(x);
 		System.out.println(r);
+		
+//		System.out.println(Short.MAX_VALUE); // 32767
+//		System.out.println(Short.MIN_VALUE); // -32768
+//
+//		System.out.println(Integer.MAX_VALUE); // 2147483647
+//		System.out.println(Integer.MIN_VALUE); // -2147483648
+//		long sum = 1;
+//		for (int i=1; i<=31; i++) {
+//		    sum = sum * 2;
+//			System.out.println(i + "\t" + sum);
+//		}
+		
 	}
 }

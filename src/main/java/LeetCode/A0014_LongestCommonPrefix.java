311701c0137d12c1bdbafcaa933f0e9493b626cf
rocky.wang <rockywang101@gmail.com>
2019-11-25 23:29:10 +0800

    add A0014

--- src/main/java/LeetCode/A0014_LongestCommonPrefix.java
+++ src/main/java/LeetCode/A0014_LongestCommonPrefix.java
@@ -2,21 +2,61 @@ package LeetCode;
 
 public class A0014_LongestCommonPrefix {
 
+	// 看了 binary search 的說明後，自己來寫看看
+	// 其實第一次 submit 是 1 秒
+	// Runtime: 0 ms, faster than 100.00% of Java online submissions for Longest Common Prefix.
+	// Memory Usage: 36.1 MB, less than 100.00% of Java online submissions for Longest Common Prefix.
 	public static String longestCommonPrefix(String[] strs) {
+		
+		if (strs.length == 0)
+			return "";
+		
+		String word = strs[0];
+		int low = 0;
+		int high = word.length() - 1;
+		
+		String prefix = "";
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			String testPrefix = word.substring(0, mid+1);
+			boolean isMatched = true;
+			for (int i=1; i<strs.length; i++) {
+				if (! strs[i].startsWith(testPrefix)) {
+					isMatched = false;
+					break;
+				}
+			}
+			
+			if (isMatched) {
+				low = mid + 1;
+				prefix = testPrefix;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		
+		return prefix;
+	}
+	
+	
+	// 奇怪不知道早上去哪 copy 到這版，現在找不到了，這版算是最好懂效能也不錯的
+	// Runtime: 0 ms, faster than 100.00% of Java online submissions for Longest Common Prefix.
+	// Memory Usage: 37.9 MB, less than 80.70% of Java online submissions for Longest Common Prefix.
+	public static String longestCommonPrefixV2(String[] strs) {
 	    
 		if (strs.length == 0) 
 	    	return "";
 	    
-		String pre = strs[0];
+		String prefix = strs[0];
 	    
 		for (int i = 1; i < strs.length; i++)
-	        while(strs[i].indexOf(pre) != 0)
-	            pre = pre.substring(0,pre.length()-1);
+	        while(strs[i].indexOf(prefix) != 0)
+	            prefix = prefix.substring(0, prefix.length()-1);
 	    
-	    return pre;
+	    return prefix;
 	}
 	
-	// 看了解答，發現這題竟然也是 binary search !
 	
 	// 錯很多次，而且分數還超差
 	// Runtime: 2 ms, faster than 16.23% of Java online submissions for Longest Common Prefix.
@@ -58,10 +98,14 @@ public class A0014_LongestCommonPrefix {
     }
     
     public static void main(String[] args) {
-		String[] strs = new String[] {"a"};
+		String[] strs = new String[] {"leets", "leetcode", "leetc", "leeds"};
 		
 		String pre = longestCommonPrefix(strs);
-		System.out.println(pre);
+		System.out.println("answer: " + pre);
+		
+		strs = new String[] {"c","acc","ccc"};
+		pre = longestCommonPrefix(strs);
+		System.out.println("answer: " + pre);
     	
 	}
 }

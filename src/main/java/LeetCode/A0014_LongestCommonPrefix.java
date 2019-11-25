72d9157246a0b20718564fa47fb2476946c316f7
rocky.wang <rockywang101@gmail.com>
2019-11-25 09:14:30 +0800

    add A0014 ans

--- src/main/java/LeetCode/A0014_LongestCommonPrefix.java
+++ src/main/java/LeetCode/A0014_LongestCommonPrefix.java
@@ -2,12 +2,26 @@ package LeetCode;
 
 public class A0014_LongestCommonPrefix {
 
+	public static String longestCommonPrefix(String[] strs) {
+	    
+		if (strs.length == 0) 
+	    	return "";
+	    
+		String pre = strs[0];
+	    
+		for (int i = 1; i < strs.length; i++)
+	        while(strs[i].indexOf(pre) != 0)
+	            pre = pre.substring(0,pre.length()-1);
+	    
+	    return pre;
+	}
+	
 	// 看了解答，發現這題竟然也是 binary search !
 	
 	// 錯很多次，而且分數還超差
 	// Runtime: 2 ms, faster than 16.23% of Java online submissions for Longest Common Prefix.
 	// Memory Usage: 38.1 MB, less than 80.12% of Java online submissions for Longest Common Prefix.
-    public static String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefixV1(String[] strs) {
      
     	if (strs.length == 0 || strs[0].length() == 0)
     		return "";

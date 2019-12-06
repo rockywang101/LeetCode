5f5ed5f516966358124584e31f9f027721f11b1f
rocky.wang <rockywang101@gmail.com>
2019-12-06 22:53:22 +0800

    add A0083

---
+++ src/main/java/A0083_RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,37 @@
+/**
+ * 操作 ListNode 練習題
+ * 
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * 
+ * @author rocky.wang
+ */
+public class A0083_RemoveDuplicatesfromSortedList {
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted List.
+    // Memory Usage: 37.8 MB, less than 41.67% of Java online submissions for Remove Duplicates from Sorted List.
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode tmpHead = head;
+        
+        while (tmpHead != null && tmpHead.next != null) {
+            
+            if (tmpHead.val == tmpHead.next.val) {
+                tmpHead.next = tmpHead.next.next; // 跳過重覆的數字 (這時還不能更換 head 因為下一個還是可能是重覆的)
+            }
+            else {
+                tmpHead = tmpHead.next; // 沒問題，換下一個 Node
+            }
+        }
+        
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}

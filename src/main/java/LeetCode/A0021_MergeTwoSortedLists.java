4527ba236090ad537bbb79fbffb2b106870737e1
rocky <rocky@rocky-UX310UAK>
2019-12-11 16:44:43 +0800

    add ListNode.java

--- src/main/java/LeetCode/A0021_MergeTwoSortedLists.java
+++ src/main/java/LeetCode/A0021_MergeTwoSortedLists.java
@@ -39,13 +39,4 @@ public class A0021_MergeTwoSortedLists {
         
         return rtnList;
     }
-    
-    class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
-    }
 }

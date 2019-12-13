58dc0ef718f95558b24e50973d56319967f47706
rocky.wang <rocky.wang@ehsn.com.tw>
2019-12-13 17:11:45 +0800

    fix conflict

--- src/main/java/LeetCode/A0709_ToLowerCase.java
+++ src/main/java/LeetCode/A0709_ToLowerCase.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package LeetCode;
-
-/**
- * 只是在練習 ascii code 的知識而已
- * 
- * https://leetcode.com/problems/to-lower-case/
- * @author rocky
- */
-public class A0709ToLowerCase {
-	
-	// Runtime: 0 ms, faster than 100.00% of Java online submissions for To Lower Case.
-	// Memory Usage: 33.8 MB, less than 98.70% of Java online submissions for To Lower Case.
-    public String toLowerCase(String str) {
-
-    	char[] charArray = str.toCharArray();
-    	for (int i = 0, length = charArray.length; i < length; i++) {
-    		if (charArray[i] >= 65 && charArray[i] <= 90) {
-    			charArray[i] += 32;
-    		}
-    	}
-    	
-    	return String.valueOf(charArray);
-    }
-    
-    public static void main(String[] args) {
-		String str = new A0709ToLowerCase().toLowerCase("ABC");
-		System.out.println(str);
-	}
-}
-=======
 package LeetCode;
 
 /**
@@ -58,4 +27,3 @@ public class A0709_ToLowerCase {
 		System.out.println(str);
 	}
 }
->>>>>>> branch 'master' of https://github.com/rockywang101/LeetCode.git

18869ee98390ceb4362edb5573a09b487ddc9f5c
rocky.wang <rocky.wang@ehsn.com.tw>
2019-12-25 17:15:51 +0800

    remove unused code

--- src/main/java/LeetCode/A0100_SameTree.java
+++ src/main/java/LeetCode/A0100_SameTree.java
@@ -3,19 +3,12 @@ package LeetCode;
 /**
  * https://leetcode.com/problems/same-tree/
  * 
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- * 
  * @author rocky
  */
 public class A0100_SameTree {
     
-    // 簡化邏輯版本
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
+    // Memory Usage: 34.1 MB, less than 100.00% of Java online submissions for Same Tree.
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
         // 都是空
@@ -30,36 +23,20 @@ public class A0100_SameTree {
         
         return isSameTree(p.right, q.right) && isSameTree(p.left, q.left); 
     }
+
     
-    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
-    // Memory Usage: 34.1 MB, less than 100.00% of Java online submissions for Same Tree.
-    public boolean isSameTreeV1(TreeNode p, TreeNode q) {
-        // [] vs []
-        if (p == null && q == null)
-            return true;
-        // 比對本身的值
-        if ((p == null && q != null) || (p != null && q == null) || p.val != q.val)
-            return false;
-        // 比對都沒有小孩，代表相同
-        if (p.left == null && p.right == null && q.left == null && q.right == null)
-            return true;
-        // 左右是否 null vs 有值，代表不同
-        if ((p.left == null && q.left != null) || (p.left != null && q.left == null)
-                || (p.right == null && q.right != null) || (p.right != null && q.right == null))
-            return false;
+    public boolean isSameTreeWhileVersion(TreeNode p, TreeNode q) {
         
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
-    }
-    
-    
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
+        while (p != null && q != null) {
+            
+            if (p == null || q == null)
+                return false;
+            
+            if (p.val != q.val)
+                return false;
+            
         }
+        
+        return true; // finally p and q are all null and no different
     }
-
 }

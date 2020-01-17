57449b0cb6edaadc1043e835b70e0cffa7df27b7
rocky <rocky@rockyde-mbp>
2020-01-17 23:36:54 +0800

    add 206 solution

--- src/main/java/LeetCode/A0226_InvertBinaryTree_V2.java
+++ src/main/java/LeetCode/A0226_InvertBinaryTree_V2.java
@@ -3,7 +3,10 @@ package LeetCode;
 /**
  * https://leetcode.com/problems/invert-binary-tree/
  * 
- * iterative version
+ * recursive version by myself
+ * 
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
+ * Memory Usage: 40.1 MB, less than 5.10% of Java online submissions for Invert Binary Tree.
  * 
  * @author rocky
  */
@@ -11,11 +14,24 @@ public class A0226_InvertBinaryTree_V2 {
 
     public TreeNode invertTree(TreeNode root) {
 
-        if (root == null) 
+        if (root == null)
             return null;
-
         
+        invert(root);
         
         return root;
+    }
+    
+    private void invert(TreeNode node) {
+        // 左邊的小孩還有小孩的話，先讓下面的小孩去做交換
+        if (node.left != null && (node.left.left != null || node.left.right != null))
+            invert(node.left);
+        // 右邊的小孩還有小孩的話，先讓下面的小孩去做交換
+        if (node.right != null && (node.right.left != null || node.right.right != null))
+            invert(node.right);
+        // 交換
+        TreeNode tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
     }
 }
--- src/main/java/LeetCode/A0226_InvertBinaryTree_V3.java
+++ src/main/java/LeetCode/A0226_InvertBinaryTree_V3.java
@@ -1,38 +1,46 @@
 package LeetCode;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode.com/problems/invert-binary-tree/
  * 
- * recursive version by myself
+ * iterative version
  * 
  * Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
- * Memory Usage: 40.1 MB, less than 5.10% of Java online submissions for Invert Binary Tree.
+ * Memory Usage: 40 MB, less than 5.10% of Java online submissions for Invert Binary Tree.
  * 
  * @author rocky
  */
 public class A0226_InvertBinaryTree_V3 {
 
     public TreeNode invertTree(TreeNode root) {
+    	if (root == null)
+    		return null;
 
-        if (root == null)
-            return null;
-        
-        invert(root);
-        
-        return root;
+    	LinkedList<TreeNode> keepList = new LinkedList<TreeNode>();
+    	keepList.add(root);
+    	
+    	while (keepList.size() > 0) {
+    		for (int i=0; i<keepList.size(); i++) {
+
+    			TreeNode node = keepList.removeFirst();
+    			
+    			if (node.left != null || node.right != null) {
+    				// exchange
+    				TreeNode tmp = node.left;
+    				node.left = node.right;
+    				node.right = tmp;
+    				
+    				if (node.left != null)
+    					keepList.add(node.left);
+    				if (node.right != null)
+    					keepList.add(node.right);
+    			}
+    		}
+    	}
+    	
+    	return root;
     }
     
-    private void invert(TreeNode node) {
-        // 左邊的小孩還有小孩的話
-        if (node.left != null && (node.left.left != null || node.left.right != null))
-            invert(node.left);
-        // 右邊的小孩還有小孩的話
-        if (node.right != null && (node.right.left != null || node.right.right != null))
-            invert(node.right);
-
-        // 交換
-        TreeNode tmp = node.left;
-        node.left = node.right;
-        node.right = tmp;
-    }
 }

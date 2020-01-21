86fc8c5ee7c72009415c789843c3c200e2db288e
rocky.wang <rocky.wang@ehsn.com.tw>
2020-01-21 17:46:49 +0800

    add 700

---
+++ src/main/java/LeetCode/A0700_SearchInABinarySearchTree_V1.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ * 
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Search in a Binary Search Tree.
+ * Memory Usage: 48 MB, less than 8.89% of Java online submissions for Search in a Binary Search Tree.
+ * 
+ * @author rocky
+ */
+public class A0700_SearchInABinarySearchTree_V1 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if (root == null)
+            return null;
+        
+        if (root.val == val)
+            return root;
+        
+        if (root.val > val)
+            return searchBST(root.left, val);
+        else
+            return searchBST(root.right, val);
+    }
+}

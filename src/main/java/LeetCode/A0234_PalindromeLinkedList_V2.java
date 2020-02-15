f28e459106dcaa3ee744b8ae9d2c588283000d84
rocky <rocky@rockyde-mbp>
2020-02-15 15:31:31 +0800

    #234

--- src/main/java/LeetCode/A0234_PalindromeLinkedList_V2.java
+++ src/main/java/LeetCode/A0234_PalindromeLinkedList_V2.java
@@ -4,9 +4,10 @@ package LeetCode;
  * https://leetcode.com/problems/palindrome-linked-list/
  * 
  * 快慢指標，O(n) time, O(1) space
+ * 很怪的是，寫法是 O(1) space，但 Memory 使用量的評分並沒有差異太多，拿別人的解法去 submit 也是一樣
  * 
  * Runtime: 0 ms, faster than 100.00% of Java online submissions for Palindrome Linked List.
- * Memory Usage: 42.2 MB, less than 28.05% of Java online submissions for Palindrome Linked List.
+ * Memory Usage: 42.2 MB, less than 29.27% of Java online submissions for Palindrome Linked List.
  * 
  * @author rocky
  */
@@ -19,28 +20,25 @@ public class A0234_PalindromeLinkedList_V2 {
         
         ListNode slow = head;
         ListNode fast = head.next;
-        ListNode curr = head;
-        
+        ListNode right = head.next;
         while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            curr = slow;
+        	fast = fast.next.next;
+        	ListNode tmp = right;
+        	right = tmp.next;
+        	tmp.next = slow;
+        	slow = tmp;
         }
-        
-        // 偶數個 nodes
-        if (fast.next == null)
-            curr = curr.next;
+
         // 奇數個 nodes
-        else
-            curr = curr.next.next;
+        if (fast.next != null)
+            right = right.next;
         
-        slow = head;
-        while (curr != null) {
-            if (slow.val != curr.val)
+        while (right != null) {
+            if (slow.val != right.val)
                 return false;
-            
+
             slow = slow.next;
-            curr = curr.next;
+            right = right.next;
         }
         
         return true;

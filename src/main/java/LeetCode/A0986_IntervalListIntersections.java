5077a3f75466a31ad2cf89365782d0c1992817ec
rocky <rocky@rockyde-mbp>
2020-02-24 00:46:12 +0800

    #986 to be started

---
+++ src/main/java/LeetCode/A0986_IntervalListIntersections.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+/**
+ * https://leetcode.com/problems/interval-list-intersections/
+ * 
+ * @author rocky
+ */
+public class A0986_IntervalListIntersections {
+    
+	public int[][] intervalIntersection(int[][] A, int[][] B) {
+    
+		return null;
+    }
+}

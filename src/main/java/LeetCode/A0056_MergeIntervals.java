3d2d2a0f8d66930a5f06bd939d7369e3c3625e37
rocky <rocky@rockyde-mbp>
2020-02-24 00:45:13 +0800

    #56 to be start

---
+++ src/main/java/LeetCode/A0056_MergeIntervals.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+/**
+ * https://leetcode.com/problems/merge-intervals/
+ * 
+ * @author rocky
+ */
+public class A0056_MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+     
+    	
+    	return null;
+    }
+}
